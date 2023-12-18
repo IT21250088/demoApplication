663ba984d99dd6bd2ff6bc053b35a46705ad7311
Sahan Gunawardena <IT21250088@my.sliit.lk>
2023-12-18 11:44:19 +0530

    Add project files.

---
+++ WebApplication1/Controllers/TodoAppController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoAppController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public TodoAppController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetNotes")]
+        public JsonResult GetNotes()
+        {
+            string query = @"
+                    select NoteId, NoteTitle, NoteDescription from dbo.Notes";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("TodoAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
